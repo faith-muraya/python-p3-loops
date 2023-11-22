451506db7804c54191d2243cc629038d5c70c5a9
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-11-22 16:23:01 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+  i = 10
+  while i >= 1:
+     print(i)
+     i -= 1
+     print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def square_integers(lst):
+    squared_lst = [x**2 for x in lst]
+    return squared_lst
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   for i in range(1, 101):
+       if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+       elif i % 3 == 0:
+            print("Fizz")
+       elif i % 5 == 0:
+             print("Buzz")
+       else:
+             print (i)
